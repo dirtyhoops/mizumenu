a9a3bdbf70321279cd3cc85ec16c3f81012cc706
Daryll <dosis@csumb.edu>
2021-07-15 00:54:10 -0700

    save point

---
+++ build/precache-manifest.a17a5d835bd542f6df095e098fde8aa6.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3e9e7a3c610ae47a38ff6d00067b36",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d72fcfe9c049a1ca340",
+    "url": "./static/css/main.7403c7f7.chunk.css"
+  },
+  {
+    "revision": "f9e31e3fe2309cb2abb3",
+    "url": "./static/js/2.36f49dd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.36f49dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d72fcfe9c049a1ca340",
+    "url": "./static/js/main.152ee4b5.chunk.js"
+  },
+  {
+    "revision": "9a3918eefd4d2c4fb634",
+    "url": "./static/js/runtime-main.d6579ed3.js"
+  },
+  {
+    "revision": "ba7c86047e95bf842a42ac589e2e64d0",
+    "url": "./static/media/alaska.ba7c8604.jpg"
+  },
+  {
+    "revision": "d3af2e9dfae00ccd0d5199bc67de6740",
+    "url": "./static/media/albacoredelight.d3af2e9d.jpg"
+  },
+  {
+    "revision": "39d036877780f7600d834093d976f437",
+    "url": "./static/media/assortedtempura.39d03687.jpg"
+  },
+  {
+    "revision": "0af46a45c0c5bae3809aa93059c07fef",
+    "url": "./static/media/beefbulgogi.0af46a45.jpg"
+  },
+  {
+    "revision": "5a6e1c414225a283bf115fc3218ed255",
+    "url": "./static/media/butterfly.5a6e1c41.jpg"
+  },
+  {
+    "revision": "f14cdce8190f974a680fa78675da2a47",
+    "url": "./static/media/cajuntuna.f14cdce8.jpg"
+  },
+  {
+    "revision": "55d0c6fd68928763f74245bc62b43da7",
+    "url": "./static/media/calamari.55d0c6fd.jpg"
+  },
+  {
+    "revision": "0b437a4a4364addbc20d9d0f8b9acdaa",
+    "url": "./static/media/cherryblossom.0b437a4a.jpg"
+  },
+  {
+    "revision": "b58fcf5f6dd94aef467ae480ba71c35e",
+    "url": "./static/media/chickenbulgogi.b58fcf5f.jpg"
+  },
+  {
+    "revision": "8ac59aaf2249d1002814c5904a267ba7",
+    "url": "./static/media/chickenkatsu.8ac59aaf.jpg"
+  },
+  {
+    "revision": "27e4b65335c001d7a27deefd4aa88736",
+    "url": "./static/media/chirashi.27e4b653.jpg"
+  },
+  {
+    "revision": "e09cbf587a396c59b973cade797a80ee",
+    "url": "./static/media/citrussearedsalmon.e09cbf58.jpg"
+  },
+  {
+    "revision": "4fcaa0d224c9c7bc9fc0ed8883af9250",
+    "url": "./static/media/crazydragon.4fcaa0d2.jpg"
+  },
+  {
+    "revision": "c35e7d8c3318e9790e34b4dfb396d3fd",
+    "url": "./static/media/crispyrice.c35e7d8c.jpg"
+  },
+  {
+    "revision": "a99f0df7432c27e3549d09a4172f198b",
+    "url": "./static/media/croquete.a99f0df7.jpg"
+  },
+  {
+    "revision": "3323b2933b3d64cf42fc8efaf21b015f",
+    "url": "./static/media/dragon.3323b293.jpg"
+  },
+  {
+    "revision": "c50dff092832b11b693398cec8427482",
+    "url": "./static/media/galbi.c50dff09.jpg"
+  },
+  {
+    "revision": "1b02db23438eab1cea4b9bb6349d99b4",
+    "url": "./static/media/greendragon.1b02db23.jpg"
+  },
+  {
+    "revision": "b32cdb7936f017f2c66efd901b69a7c1",
+    "url": "./static/media/gyoza.b32cdb79.jpg"
+  },
+  {
+    "revision": "6df785114a19932d042120d61779b384",
+    "url": "./static/media/hamachikama.6df78511.jpg"
+  },
+  {
+    "revision": "f87afa337c3cdd778c52b34389c8bdfd",
+    "url": "./static/media/honeymoonspecial.f87afa33.jpg"
+  },
+  {
+    "revision": "d3c5d1096cc00e7cc3e2bb58e9dc2a27",
+    "url": "./static/media/hotmango.d3c5d109.jpg"
+  },
+  {
+    "revision": "0ff80f350740d08c29099541309d8b3b",
+    "url": "./static/media/houseboat.0ff80f35.jpg"
+  },
+  {
+    "revision": "90892b2ee11260bda6fd85066cec11c5",
+    "url": "./static/media/hurricane.90892b2e.jpg"
+  },
+  {
+    "revision": "fad0db0145b9e23887de02d36594e082",
+    "url": "./static/media/kakeudon.fad0db01.png"
+  },
+  {
+    "revision": "d7fb116d04a44b92b315406e3372c035",
+    "url": "./static/media/misoblackcod.d7fb116d.jpg"
+  },
+  {
+    "revision": "7386775d366cf5e73fef0bf4ecb84a00",
+    "url": "./static/media/mizubomb.7386775d.jpg"
+  },
+  {
+    "revision": "cb8dbb300f8f05923c39169a9a888b1a",
+    "url": "./static/media/mizuhousesalad.cb8dbb30.jpg"
+  },
+  {
+    "revision": "8c6b40e544e45a4b2c8bca0d1cf6858b",
+    "url": "./static/media/mizujumbo.8c6b40e5.jpg"
+  },
+  {
+    "revision": "c6c581970d79d4c39dda42136f7d24b5",
+    "url": "./static/media/mussels.c6c58197.jpg"
+  },
+  {
+    "revision": "cf0f6ef280318145be2177988813fbdd",
+    "url": "./static/media/nabeyakiudon.cf0f6ef2.jpg"
+  },
+  {
+    "revision": "210bd4ab5db9a4d30c64b3ac62ad0abe",
+    "url": "./static/media/popcornlobster.210bd4ab.jpg"
+  },
+  {
+    "revision": "2ee25200cce836df22f3bcba1146861f",
+    "url": "./static/media/porkdonburi.2ee25200.jpg"
+  },
+  {
+    "revision": "77d5b5c6448326439b3b1b3f3495f611",
+    "url": "./static/media/rainbow.77d5b5c6.jpg"
+  },
+  {
+    "revision": "4efd1dba342434ac50ea2ff4b99abd45",
+    "url": "./static/media/reddragon.4efd1dba.jpg"
+  },
+  {
+    "revision": "07c0934c7416dad98c7f195c761c16f1",
+    "url": "./static/media/sakura.07c0934c.jpg"
+  },
+  {
+    "revision": "8c8d746febc332d83a92e828cf4820f1",
+    "url": "./static/media/salmoncarpaccio.8c8d746f.jpg"
+  },
+  {
+    "revision": "fbdf293800ff74a1f756f9efb04540af",
+    "url": "./static/media/salmonteriyaki.fbdf2938.jpg"
+  },
+  {
+    "revision": "693751c8a72011c011b9c251816f88d3",
+    "url": "./static/media/sashimininepcs.693751c8.jpg"
+  },
+  {
+    "revision": "21e774848bb8f9e6598d8c0b9595ee8e",
+    "url": "./static/media/seaweed.21e77484.png"
+  },
+  {
+    "revision": "fcf5a70adb57f8554690aa180496bbdc",
+    "url": "./static/media/signatureboat.fcf5a70a.jpg"
+  },
+  {
+    "revision": "c4753b2a175f75efdb6b3c0b955aac91",
+    "url": "./static/media/spicycrunch.c4753b2a.jpg"
+  },
+  {
+    "revision": "9fa181c0b3de7097375f08abf3cae8e1",
+    "url": "./static/media/spicypopcorn.9fa181c0.jpg"
+  },
+  {
+    "revision": "f76d401d22ee3f01cbb0e5352230e724",
+    "url": "./static/media/spicyporkbulgogi.f76d401d.jpg"
+  },
+  {
+    "revision": "9750c339d8fd3434e8730f53b50b03f8",
+    "url": "./static/media/spicytunasalad.9750c339.jpg"
+  },
+  {
+    "revision": "9714f7041156b75ee4c3a4ebed4a0fab",
+    "url": "./static/media/sunkissed.9714f704.jpg"
+  },
+  {
+    "revision": "55705053b4917fa7ef77e4cca11b90d7",
+    "url": "./static/media/supergodzilla.55705053.jpg"
+  },
+  {
+    "revision": "28044c24502f5445912aec1006ccb472",
+    "url": "./static/media/superspider.28044c24.jpg"
+  },
+  {
+    "revision": "92391d52e17a58922f02d758c11e0666",
+    "url": "./static/media/sushidinner.92391d52.jpg"
+  },
+  {
+    "revision": "d30ade94af91a2d24c7036fa53ded758",
+    "url": "./static/media/sushisashimidinner.d30ade94.jpg"
+  },
+  {
+    "revision": "7e4e7fe4620d77401ddcee320c5509c2",
+    "url": "./static/media/tempuraudon.7e4e7fe4.png"
+  },
+  {
+    "revision": "58fe474e16d00936ba96daed942b4f3d",
+    "url": "./static/media/titanic.58fe474e.jpg"
+  },
+  {
+    "revision": "8f5f8472d43f4ac36cb4e60f58bf099d",
+    "url": "./static/media/tofu.8f5f8472.jpg"
+  },
+  {
+    "revision": "5d73852064f81e0dd05bbd15cf8011f7",
+    "url": "./static/media/tunatartar.5d738520.jpg"
+  },
+  {
+    "revision": "e18bb0da0d34fa78ffb43a616803032a",
+    "url": "./static/media/unagilover.e18bb0da.jpg"
+  },
+  {
+    "revision": "13a910f3d2ef7d787b9b0c7146ee5b27",
+    "url": "./static/media/yamiyami.13a910f3.jpg"
+  },
+  {
+    "revision": "a0b7447f391090d84eafd595f8c0e6af",
+    "url": "./static/media/yellowtailcrudo.a0b7447f.jpg"
+  },
+  {
+    "revision": "3a98c33d10606e35ba623402597c1a4c",
+    "url": "./static/media/yellowtailspecial.3a98c33d.jpg"
+  },
+  {
+    "revision": "17dd0f9a0e52dbdb108bf9b80642180b",
+    "url": "./static/media/yellowtailtartar.17dd0f9a.jpg"
+  }
+]);
